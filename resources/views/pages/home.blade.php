45ca89b628311f28c056e7176f813c7e1ae4a77b
Ahmed Al-Gallad <ahmed.algalladd@gmail.com>
2018-06-20 18:06:56 +0200

    Added Stronghold Container part

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -18,4 +18,36 @@
 
 <!-- End of Image Slider -->
 
+<!-- Begining of stronghold container -->
+<div class="container-fluid strongholdContainer" style="border-top: 1px solid white;">
+	<div class="row text-center">
+
+		<div class="col-sm-4" id="bluepartCol1">
+			<img src="images/rootcanal.png" id="bluepartImg1">
+			<div class="mt-20">
+				<h4 class="bluePartHeader">Root Canals</h4>
+				<p class="bluePartParagraph">Affordable Prices</p>
+			</div>
+		</div>
+
+		<div class="col-sm-4" id="bluepartCol2">
+			<img src="images/routinecheckups.png" id="bluepartImg2">
+			<div class="mt-20">
+				<h4 class="bluePartHeader">Routine Check-ups</h4>
+				<p class="bluePartParagraph">Comprehensive dental check-ups</p>
+			</div>
+		</div>
+
+		<div class="col-sm-4" id="bluepartCol3">
+			<img src="images/teathwhitening.png" id="bluepartImg3">
+			<div class="mt-20">
+				<h4 class="bluePartHeader">Teeth Whitening</h4>
+				<p class="bluePartParagraph">High Quality teeth whitening</p>
+			</div>
+		</div>
+
+	</div>
+</div>
+<!-- End of Stronghold Container -->
+
 @endsection
