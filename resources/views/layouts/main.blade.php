287357282134c46e5ea80229a6d67c63dfccb85e
Ahmed Al-Gallad <ahmed.algalladd@gmail.com>
2018-06-23 21:25:35 +0200

    Added Images, New slider and updated stylesheets

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -18,84 +18,152 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/generic.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/font-awesome-4.7.0/css/font-awesome.min.css') }}">
-    
 
 </head>
 
 <body>
 
-    <div class="container-fluid" id="top-NavPart">
-        <div class="container">
-            <div class="row" id="top-NavPartRow">
+   <!-- Begining of top navpart container -->
+   <div class="container-fluid" id="top-NavPart">
+    <div class="container">
+        <div class="row" id="top-NavPartRow">
 
+
+            <div class="col-md-3 col-sm-3 col-xs-12 top-NavPart-contact">
+                <p class="top-NavPart-contactP">
+                    <i class="fa fa-clock-o"></i> From 11:00 am To 11:00 pm  </p>
+                </div>
 
                 <div class="col-md-3 col-sm-3 col-xs-12 top-NavPart-contact">
                     <p class="top-NavPart-contactP">
-                        <i class="fa fa-clock-o"></i> From 11:00 am To 11:00 pm  </p>
+                        <i class="fa fa-envelope-open"></i> <a href="#">info@whitesmileclinic-eg.com</a>  </p>
                     </div>
 
                     <div class="col-md-3 col-sm-3 col-xs-12 top-NavPart-contact">
                         <p class="top-NavPart-contactP">
-                            <i class="fa fa-envelope-open"></i> <a href="#">info@whitesmileclinic-eg.com</a>  </p>
+                            <i class="fa fa-mobile-phone fa-lg"></i> 01096150382 - 0128388225  </p>
                         </div>
 
-                        <div class="col-md-3 col-sm-3 col-xs-12 top-NavPart-contact">
-                            <p class="top-NavPart-contactP">
-                                <i class="fa fa-mobile-phone fa-lg"></i> 01096150382 - 0128388225  </p>
-                            </div>
-
-                            <div class="col-md-3 col-sm-3 col-xs-12" id="smIconsDiv">
-                                <a href="#"><i class="fa fa-facebook" id="fbIcon"></i></a>
-                                <a href="#"><i class="fa fa-youtube" id="ytIcon"></i></a>
-                                <a href="#"><i class="fa fa-instagram" id="ig-Icon"></i></a>
-                                <a href="#"><i class="fa fa-linkedin" id="liIcon"></i></a>
-                                <a href="#"><i class="fa fa-twitter" id="twIcon"></i></a>
-                            </div>
+                        <div class="col-md-3 col-sm-3 col-xs-12" id="smIconsDiv">
+                            <a href="#"><i class="fa fa-facebook" id="fbIcon"></i></a>
+                            <a href="#"><i class="fa fa-youtube" id="ytIcon"></i></a>
+                            <a href="#"><i class="fa fa-instagram" id="ig-Icon"></i></a>
+                            <a href="#"><i class="fa fa-linkedin" id="liIcon"></i></a>
+                            <a href="#"><i class="fa fa-twitter" id="twIcon"></i></a>
                         </div>
                     </div>
                 </div>
-                <div class="container" id="navbarContainer">
-                    <nav class="navbar navbar-expand-lg mainNav fixed">
-                        <a class="navbar-brand" id="websiteTitle" href="#">Al Nahass Clinic</a>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarNav">
-                            <ul class="navbar-nav">
-                                <li class="nav-item">
-                                    <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">About Us</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Services</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Dental Studio</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Gallery</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">News</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Contact Us</a>
-                                </li>
-                            </ul>
+            </div>
+
+            <!--    End of top navpart container -->
+
+
+            <!-- Begining of Navbar Container -->
+            <div class="container" id="navbarContainer">
+                <nav class="navbar navbar-expand-lg mainNav fixed">
+                    <a class="navbar-brand" id="websiteTitle" href="#">Al Nahass Clinic</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="fa fa-bars"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">About Us</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Services</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Dental Studio</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Gallery</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">News</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contact Us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+            <!--    End of Navbar container  -->              
+
+
+            <!-- Begininig of mobileOnly Container -->
+            <div class="container-fluid" id="mobileOnly">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h5><i class="fa fa-phone"></i> Call Us</h5>
+                        <p>+202 33 45 12 53 / +2012 83 88 22 54</p>
+                    </div>
+                    <div class="col-sm-12">
+                        <h5><i class="fa fa-clock-o"></i> Opening Hours</h5>
+                        <p>From 11:00 am To 11:00 pm</p>
+                    </div>
+                    <div class="col-sm-12">
+                        <h5><i class="fa fa-calendar"></i> Book an appointment</h5>
+                        <p><a href="#">Your perfect smile is one click away!</a></p>
+                    </div>
+                </div>
+            </div>
+            <!-- End of Mobile Only container -->
+            <div>
+                @yield('content')
+            </div>
+            <!-- Begining of Footer -->
+            <div class="container footer">
+                <div class="row mt-20">
+                    <div class="col-md-4">
+                        <h4>Clinic 1:</h4>
+                        <div class="footerPs">
+                            <p><i class="fa fa-map-marker"></i> 21 El Batal Ahmed Abd El-Aziz</p>
+                            <p><i class="fa fa-phone"></i> +202 33 45 12 53</p>
+                            <p><i class="fa fa-mobile-phone fa-lg"></i> +2010 96 15 03 82</p>
+                            <p><i class="fa fa-envelope"></i> elbatal@alnahassclinic.com</p>
                         </div>
-                    </nav>
+                    </div> 
+                    <div class="col-md-4">
+                        <h4>Clinic 2:</h4>
+                        <div class="footerPs">
+                            <p><i class="fa fa-map-marker"></i> 21 El Batal Ahmed Abd El-Aziz</p>
+                            <p><i class="fa fa-phone"></i> +202 33 45 12 53</p>
+                            <p><i class="fa fa-mobile-phone fa-lg"></i> +2010 96 15 03 82</p>
+                            <p><i class="fa fa-envelope"></i> elbatal@alnahassclinic.com</p>
+                        </div>
+                    </div> 
+                    <div class="col-md-4 footerSm">
+                        <h4>Follow Us:</h4>
+                        <a href="#"><i class="fa fa-facebook fbIcon"></i></a>
+                        <a href="#"><i class="fa fa-twitter twIcon"></i></a>
+                        <a href="#"><i class="fa fa-instagram ig-icon"></i></a>
+                        <a href="#"><i class="fa fa-youtube ytIcon"></i></a>
+                    </div> 
                 </div>
-                <div>
-                    @yield('content')
+            </div>
+            <!-- End of Footer -->
+            <!-- Begining of Copyright container -->
+            <div class="container-fluid copyrightContainer">
+                <div class="container copyrightContainer">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-12 col-xs-12">
+                            <span>Copyright © 2018 Al-Nahass Clinic. All Rights Reserved. Developed By 47-Flakes</span>
+                        </div>
+                    </div>
                 </div>
+            </div>
 
-                <script src="{{ asset("js/app.js") }}"></script>
-                 <script src="{{ asset("js/js-image-slider.js") }}"></script>
-                @stack('scripts')
+            <!-- End of Copyright container -->
 
-            </body>
-            </html>
+
+            <script src="{{ asset("js/app.js") }}"></script>
+            @stack('scripts')
+
+        </body>
+        </html>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,15 +3,60 @@
 
 @section('content')
 <!-- Begining of Image Slider -->
-
 <div class="container-fluid">
 	<div class="row">
-		<div id="sliderFrame">
-			<div id="slider">
-				<img src="https://whitesmileclinic-eg.com/wp-content/uploads/2017/07/slider-003.jpg" />
-				<img src="https://whitesmileclinic-eg.com/wp-content/uploads/2017/07/slider-006.jpg" alt="" />
-				<img src="https://whitesmileclinic-eg.com/wp-content/uploads/2017/07/slider-005.jpg" alt="#htmlcaption" />
+		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+			</ol>
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<img class="d-block w-100" src="images/slider-003.jpg" alt="First slide">
+					<div class="carousel-caption container carouselBox">
+
+						<h5>Al-Nahass Clinic. Changing lives one smile at a time</h5>
+						<div class="carouselBoxLine">
+						</div> 
+						<h1>Professional and highly trained dental staff</h1>
+						<p>Our dental staff includes Doctors and assistants who are highly trained. A team of talented professionals to give our clients the best treatments</p>
+						<button class="btn">Learn More</button>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<img class="d-block w-100" src="images/slider-005.jpg" alt="Second slide">
+					<div class="carousel-caption container carouselBox">
+						
+						<h5>Al-Nahass Clinic. Changing lives one smile at a time</h5>
+						<div class="carouselBoxLine">
+						</div> 
+						<h1>Dental Services for the entire family</h1>
+						<p>Our dental services are not offered only for you, but for your entire family aswell. We guarantee a perfect and healthy smile for you and your family</p>
+						<button class="btn">Learn More</button>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<img class="d-block w-100" src="images/slider-006.jpg" alt="Third slide">
+					<div class="carousel-caption container carouselBox">
+						
+						<h5>Al-Nahass Clinic. Changing lives one smile at a time</h5>
+						<div class="carouselBoxLine">
+						</div> 
+						<h1>Routine Dental Exams & Check-ups</h1>
+						<p>Keep your smile white and healthy by having the routine dental exams & check-ups. We provide best equipment and talented staff</p>
+						<button class="btn">Learn More</button>
+					</div>
+				</div>
 			</div>
+			<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
 		</div>
 	</div>
 </div>
@@ -22,7 +67,7 @@
 <div class="container-fluid strongholdContainer" style="border-top: 1px solid white;">
 	<div class="row text-center">
 
-		<div class="col-sm-4" id="bluepartCol1">
+		<div class="col-sm-4 col-xs-12" id="bluepartCol1">
 			<img src="images/rootcanal.png" id="bluepartImg1">
 			<div class="mt-20">
 				<h4 class="bluePartHeader">Root Canals</h4>
@@ -30,7 +75,7 @@
 			</div>
 		</div>
 
-		<div class="col-sm-4" id="bluepartCol2">
+		<div class="col-sm-4 col-xs-12" id="bluepartCol2">
 			<img src="images/routinecheckups.png" id="bluepartImg2">
 			<div class="mt-20">
 				<h4 class="bluePartHeader">Routine Check-ups</h4>
@@ -38,7 +83,7 @@
 			</div>
 		</div>
 
-		<div class="col-sm-4" id="bluepartCol3">
+		<div class="col-sm-4 col-xs-12" id="bluepartCol3">
 			<img src="images/teathwhitening.png" id="bluepartImg3">
 			<div class="mt-20">
 				<h4 class="bluePartHeader">Teeth Whitening</h4>
@@ -49,5 +94,6 @@
 	</div>
 </div>
 <!-- End of Stronghold Container -->
+
 
 @endsection
